8447bcd1c65425627c98ad57b86b4df75aa6da86
tami <tamiortami@gmail.com>
2025-03-31 19:45:40 +0200

    feat: added queryString

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var qs = require("querystring");
 
 function templateHTML(title, list, body) {
   return `
@@ -75,7 +76,7 @@ var app = http.createServer(function (request, response) {
       var template = templateHTML(
         title,
         list,
-        `<form action="http://localhost:3000/process_create" method="post">
+        `<form action="http://localhost:3000/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
                 <p><textarea name="description" placeholder="description"></textarea></p>
                    <p><input type="submit"></p>
@@ -84,6 +85,19 @@ var app = http.createServer(function (request, response) {
       response.writeHead(200);
       response.end(template);
     });
+  } else if (pathname === "/create_process") {
+    var body = "";
+    request.on("data", function (data) {
+      // 콜백이 실행될때마다 바디에 데이터를 추가한다
+      body = body + data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+    });
+    response.writeHead(200);
+    response.end("success");
   } else {
     response.writeHead(400);
     response.end("Not found");
